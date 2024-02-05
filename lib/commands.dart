78931962e533093b206f3bcf03a5bd88c5d5fb7e
bevz-pn <bevz.pn@ucu.edu.ua>
2024-02-05 13:41:07 +0200

    implemented ADD command

--- lib/commands.dart
+++ lib/commands.dart
@@ -1,5 +1,23 @@
 import 'package:riscambler/register.dart';
 
 class Commands {
-  static void ADD(Register register, int value1, int value2) {}
+  ///
+  /// Add a value to the given register's current value
+  ///
+  /// [value1] and [value2] are the values to be added
+  /// [register] is the register to store the result in
+  ///
+  /// Example:
+  /// ```dart
+  /// Register x1Register = Register(name: "x1", alterName: "ra");
+  /// print(ADD(x1Register, 5, 10, true)); // Output: ADD: x1 = 15
+  /// ```
+  static void ADD(Register register, int value1, int value2,
+      [bool debug = false]) {
+
+    register.setValue(value1 + value2);
+    if (debug) {
+      print("ADD: ${register.name} = ${register.getValue()}");
+    }
+  }
 }
